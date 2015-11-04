83c1d0308a71db588236793c48bcd54b3e34c7f8
Alexander Bunzya <a.bunzya@gmail.com>
2015-11-04 10:09:42 +0300

    change directive to filter

--- src/right-endings.js
+++ src/right-endings.js
@@ -1,42 +1,32 @@
 (function (angular) {
     'use strict';
 
-    angular.module('right-endings', []).directive('rightEndings', rightEndingsDirective);
+    angular.module('right-endings', []).filter('rightEndings', rightEndings);
 
-    function rightEndingsDirective(){
-        return {
-            restrict: 'E',
+    function rightEndings() {
+        return function (number, ends) {
 
-            scope: {
-                values: '=',
-                number:'='
-            },
-            template: '{{result}}',
-            controller: function($scope){
-
-                $scope.$watch('number',function(){
-                    var sEnding, i;
-                    var iNumber = $scope.number % 100;
-                    if (iNumber>=11 && iNumber<=19) {
-                        sEnding=$scope.values[2];
-                    }
-                    else {
-                        i = iNumber % 10;
-                        switch (i)
-                        {
-                            case (1): sEnding = $scope.values[0]; break;
-                            case (2):
-                            case (3):
-                            case (4): sEnding = $scope.values[1]; break;
-                            default: sEnding = $scope.values[2];
-                        }
-                    }
-                    $scope.result = sEnding;
-                })
-
-
-
+            var sEnding, i;
+            var iNumber = number % 100;
+            if (iNumber >= 11 && iNumber <= 19) {
+                sEnding = ends[2];
             }
-        };
+            else {
+                i = iNumber % 10;
+                switch (i) {
+                    case (1):
+                        sEnding = ends[0];
+                        break;
+                    case (2):
+                    case (3):
+                    case (4):
+                        sEnding = ends[1];
+                        break;
+                    default:
+                        sEnding = ends[2];
+                }
+            }
+            return sEnding;
+        }
     }
 })(angular);
